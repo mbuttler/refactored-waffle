05ba2bc324f12d92472917b72d1a664d2ad72c4a
Matthew Buttler <matthew.buttler@gmail.com>
2017-08-20 13:02:49 -0400

    first commit

---
+++ api/models/todoListModel.js
@@ -0,0 +1,24 @@
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var TaskSchema = new Schema({
+    name: {
+        type: String,
+        required: 'Task To Do'
+    },
+    Created_date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: [{
+            type: String,
+            enum: ['to start', 'started', 'completed']
+        }],
+        default: ['to start']
+    }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
